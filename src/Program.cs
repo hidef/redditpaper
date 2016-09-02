509ca6ebefd33299f8b175ae164c2e881fa2e86f
Robert Stiff <robert.stiff@just-eat.com>
2016-09-02 10:43:13 +0100

    Tidy up and make it run at intervals.

--- src/Program.cs
+++ src/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using Newtonsoft.Json.Linq;
+using System.Threading;
 
 namespace ConsoleApplication
 {
@@ -13,35 +14,47 @@ namespace ConsoleApplication
         
         public static void Main(string[] args)
         {
+            // Configuration
             string subreddit = "astrophotography";
+            string outputFolder = "tmp";
+            int interval = 2.Minutes();
 
-
+            // Preparation
             httpClient.DefaultRequestHeaders.Add("User-Agent", "RedditPaper v0.1");
 
-            HttpResponseMessage response = httpClient.GetAsync($"https://api.reddit.com/r/{subreddit}").Result;
-
-            string responseDocument = response.Content.ReadAsStringAsync().Result;
-
-            JObject document = JObject.Parse(responseDocument);
-            IEnumerable<JToken> tokens = document.SelectTokens("$.data.children..data.url");
-
-
-            if ( !Directory.Exists("tmp")) 
+            if ( !Directory.Exists(outputFolder))
             {
-                Directory.CreateDirectory("tmp");
+                Directory.CreateDirectory(outputFolder);
             }
-    
-            tokens
-                .Select(t => t.Value<string>())
-                .Where(t => t.ToLower().Contains("jpg"))
-                .ForEach(Download);
+
+            while ( true )
+            {
+                Console.WriteLine("------");
+
+                // Discovery
+                HttpResponseMessage response = httpClient.GetAsync($"https://api.reddit.com/r/{subreddit}?limit=25").Result;
+                string responseDocument = response.Content.ReadAsStringAsync().Result;
+
+                JObject document = JObject.Parse(responseDocument);
+                IEnumerable<JToken> tokens = document.SelectTokens("$.data.children..data.url");
+        
+                // Retrieval
+                tokens
+                    .Select(t => new Uri(t.Value<string>()))
+                    .Where(t => t.LocalPath.ToLower().Contains("jpg"))
+                    .ForEach(t => Download(t, outputFolder));
+
+                Thread.Sleep(interval);
+            }
         }
     
-        private static void Download(string url) 
+        private static void Download(Uri uri, string outputFolder) 
         {
-            Uri uri = new Uri(url);
+            FileInfo fi = new FileInfo(Path.Combine(outputFolder, uri.LocalPath.Substring(1)));
+            
+            if ( fi.Exists ) return ; // Skip files we've already got, get new ones only.
+
             HttpResponseMessage response = httpClient.GetAsync(uri).Result;
-            FileInfo fi = new FileInfo(Path.Combine("tmp", uri.LocalPath.Substring(1)));
             Console.WriteLine("FullName: " + fi.FullName);
             using ( var contentStream = response.Content.ReadAsStreamAsync().Result)
             using ( var writeStream = fi.OpenWrite())
@@ -51,6 +64,20 @@ namespace ConsoleApplication
         }
     }
     
+
+    public static class UnitExtensions
+    {
+        public static int Minutes(this int minutes)
+        {
+            return minutes * 60.Seconds();
+        }
+
+        public static int Seconds(this int seconds)
+        {
+            return seconds * 1000;
+        }
+    }
+
     public static class SuperLinq 
     {
         public static void ForEach<T>(this IEnumerable<T> enumeration, Action<T> action)
